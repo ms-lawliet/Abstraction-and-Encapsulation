2ec364e62aae36d6369be32b37639a2f21ef629c
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 13:38:11 +0800

    created class Fan and speed constants

--- Fan/fan_class.py
+++ Fan/fan_class.py
@@ -1,13 +1,18 @@
 # create Fan class
-# assign speed constants
-# create constructor
-# create methods,
-# set speed
-# set on or off
-# set radius
-# set color
-# get speed
-# get status (on or off)
-# get radius
-# get color
-# show properties
+class Fan:
+    # assign speed constants
+    slow = 1
+    medium = 2
+    fast = 3
+
+    # create constructor
+    # create methods,
+    # set speed
+    # set on or off
+    # set radius
+    # set color
+    # get speed
+    # get status (on or off)
+    # get radius
+    # get color
+    # show properties
