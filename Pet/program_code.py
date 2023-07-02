8fe7798b0fd0138b708451531c4ca4e1a48bbe1d
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 15:01:04 +0800

    imported pet class and user interface into program code file

--- Pet/program_code.py
+++ Pet/program_code.py
@@ -1,4 +1,9 @@
 # import pet and user interface class
+from pet_class import Pet
+from user_interface import UserInterface
+
+ui = UserInterface()
+
 # ask user for values
 # create object
 # print out results
