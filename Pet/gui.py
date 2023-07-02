a260870543f89aa971bf0c03ec6edf6030cdf193
Ms Lawliet <alexieanoya@gmail.com>
2023-07-03 06:34:10 +0800

    made modifications

--- Pet/gui.py
+++ Pet/gui.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 
 def show_image():
     img_label = tk.Label(root)
-    img_label.image = tk.PhotoImage(file="bg.jpg")
+    img_label.image = tk.PhotoImage(file="background.png")
     img_label['image'] = img_label.image
 
     img_label.pack()
