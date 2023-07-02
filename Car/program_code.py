4a674ea26549a0aeff35b5fecede79d8ef902380
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 14:23:24 +0800

    fixed some comments

--- Car/program_code.py
+++ Car/program_code.py
@@ -4,13 +4,13 @@ from car_class import Car
 # create Car object
 Car1 = Car("2017", "Mercedes Benz")
 
-# call accelerate & get speed (5 times)
+# call accelerate & get_speed (5 times)
 for i in range(0, 5):
     Car1.accelerate()
     Car1.get_speed()
     Car1.show()
 
-# call brake & get speed (5 times)
+# call brake & get_speed (5 times)
 for i in range(0, 5):
     Car1.brake()
     Car1.get_speed()
