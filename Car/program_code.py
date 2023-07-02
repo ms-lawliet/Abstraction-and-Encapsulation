39034dcfc3263f42054addd3a6572a6bb207e170
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 14:22:15 +0800

    called accelerate and get_speed methods 5 times

--- Car/program_code.py
+++ Car/program_code.py
@@ -5,4 +5,9 @@ from car_class import Car
 Car1 = Car("2017", "Mercedes Benz")
 
 # call accelerate & get speed (5 times)
+for i in range(0, 5):
+    Car1.accelerate()
+    Car1.get_speed()
+    Car1.show()
+
 # call brake & get speed (5 times)
