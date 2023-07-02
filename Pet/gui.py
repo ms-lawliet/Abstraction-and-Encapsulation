b3320d7572597b2050fcf03289e1269b6167684a
Ms Lawliet <alexieanoya@gmail.com>
2023-07-03 06:32:39 +0800

    changed bg pic

--- Pet/gui.py
+++ Pet/gui.py
@@ -1,9 +1,15 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
-root.geometry("400x400")
+root = tk.Tk()
 
-bg = PhotoImage(file="corgi.jpg")
 
+def show_image():
+    img_label = tk.Label(root)
+    img_label.image = tk.PhotoImage(file="bg.jpg")
+    img_label['image'] = img_label.image
+
+    img_label.pack()
+
+
+show_image()
 root.mainloop()
