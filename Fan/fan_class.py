47d0a0ea5e334a4c8329ac8335f43f973ae47508
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 13:37:00 +0800

    made pseudocode for fan class

---
+++ Fan/fan_class.py
@@ -0,0 +1,13 @@
+# create Fan class
+# assign speed constants
+# create constructor
+# create methods,
+# set speed
+# set on or off
+# set radius
+# set color
+# get speed
+# get status (on or off)
+# get radius
+# get color
+# show properties
