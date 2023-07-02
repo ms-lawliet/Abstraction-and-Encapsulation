ef8251d2c1865a787436d2e7b63d81ad50e564d1
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 14:39:03 +0800

    created class Pet and constructor w/ required attributes

--- Pet/pet_class.py
+++ Pet/pet_class.py
@@ -1,9 +1,14 @@
 # create pet class
-# create constructor w/ required attributes
-# create methods,
-# set_name
-# set_animal_type
-# set_age
-# get name
-# get_animal_type
-# get_age
+class Pet:
+    def __init__(self, name, animal_type, age):  # create constructor w/ required attributes
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+        
+    # create methods,
+    # set_name
+    # set_animal_type
+    # set_age
+    # get name
+    # get_animal_type
+    # get_age
