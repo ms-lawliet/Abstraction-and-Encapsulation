858be88f4dcf557dd1e1e0e4e3fb824d5f792148
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 14:55:52 +0800

    created a function to ask for animal type

--- Pet/user_interface.py
+++ Pet/user_interface.py
@@ -2,6 +2,9 @@ class UserInterface:   # create class for user interface
     def ask_pet_name(self):   # ask user for pet name,
         name = input("Enter pet name: ")
         return name
-    
-    # animal type
+
+    def ask_animal_type(self):  # animal type
+        animal_type = input("Enter animal type: ")
+        return animal_type
+
     # pet age
