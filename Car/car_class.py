a013aa6e6e927ad47efad93d4cf702288f7c87d1
Ms Lawliet <alexieanoya@gmail.com>
2023-07-02 14:06:30 +0800

    created class Car and constructor with required attributes

--- Car/car_class.py
+++ Car/car_class.py
@@ -1,7 +1,12 @@
 # create car class
-# make a constructor w/ required attributes
-# create methods
-# accelerate
-# brake
-# get speed
-# show details
+class Car:
+    def __init__(self, year_model, make, speed=0):  # make a constructor w/ required attributes
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
+
+    # create methods
+    # accelerate
+    # brake
+    # get speed
+    # show details
