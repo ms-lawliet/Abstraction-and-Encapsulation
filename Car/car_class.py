8936e8169377d16b726dcb5be620cec58a508fc3
Ms Lawliet <alexieanoya@gmail.com>
2023-07-03 21:10:32 +0800

    decided to separately show details and speed

--- Car/car_class.py
+++ Car/car_class.py
@@ -1,5 +1,8 @@
-# create car class
-class Car:
+from colorama import Fore, Back
+import time
+
+
+class Car:   # create car class
     def __init__(self, year_model, make, speed=0):  # make a constructor w/ required attributes
         self.__year_model = year_model
         self.__make = make
@@ -17,5 +20,9 @@ class Car:
     def get_speed(self):   # get speed
         return self.__speed
 
-    def show(self):   # show details
-        print(f"Car details: {self.__year_model} {self.__make}, {self.__speed} m/s")
+    def show_details(self):   # show details
+        print(Fore.MAGENTA + Back.BLACK + f"Car details: " + Fore.BLUE + f"{self.__year_model} {self.__make}" + Back.RESET)
+        time.sleep(0.5)
+
+    def show_speed(self):   # show details
+        print(Fore.RED + Back.BLACK + f"Speed: {self.__speed} m/s" + Back.RESET)
--- Car/program_code.py
+++ Car/program_code.py
@@ -1,17 +1,22 @@
 # import Car class
 from car_class import Car
+import time
 
 # create Car object
 Car1 = Car("2017", "Mercedes Benz")
+
+Car1.show_details()
 
 # call accelerate & get_speed (5 times)
 for i in range(0, 5):
     Car1.accelerate()
     Car1.get_speed()
-    Car1.show()
+    Car1.show_speed()
+    time.sleep(0.6)
 
 # call brake & get_speed (5 times)
 for i in range(0, 5):
     Car1.brake()
     Car1.get_speed()
-    Car1.show()
+    Car1.show_speed()
+    time.sleep(0.6)
